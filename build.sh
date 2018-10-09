ce26907d740f1b8b3b707b71f2614182eb8c0087
Jan Funke <funkej@janelia.hhmi.org>
2018-10-09 10:00:20 -0400

    Include gurobi binaries in conda package

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
-mkdir ${PREFIX}/lib
-mkdir ${PREFIX}/include
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/lib
+mkdir -p ${PREFIX}/include
+cp linux64/bin/{grbcluster,grbgetkey,grbprobe,grb_ts,grbtune,gurobi_cl} ${PREFIX}/bin
 cp linux64/lib/*.so ${PREFIX}/lib
 cp linux64/lib/*.a ${PREFIX}/lib
 cp -r linux64/include/*.h ${PREFIX}/include
